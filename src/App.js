062cb18a35e565fe009dd35b7910516c36491e4a
SDSamarasinghe <it20605902@my.sliit.lk>
2025-11-13 13:55:08 +0530

    Comment out ExpertsSection import and usage in App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
+// import ExpertsSection from './components/ExpertsSection';
 import Features from './components/Features';
 import Testimonials from './components/Testimonials';
 import ReelsPortfolio from './components/ReelsPortfolio';
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Header />
       <Hero />
+      {/* <ExpertsSection /> */}
       <Features />
       <ReelsPortfolio />
       <Testimonials />
